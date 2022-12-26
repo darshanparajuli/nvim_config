1dc2f22e3149a7325dd4a5b5592ee53539fb9544
Darshan Parajuli <parajulidarshan@gmail.com>
2022-12-26 15:33:48 -0800

    Initial commit

---
+++ after/plugin/nerdtree.lua
@@ -0,0 +1,8 @@
+vim.keymap.set("n", "<leader>N", vim.cmd.NERDTreeToggle)
+vim.g.NERDTreeMinimalUI = 1
+vim.g.NERDTreeAutoDeleteBuffer = 1
+vim.g.NERDTreeDirArrows = 1
+vim.g.NERDTreeDirArrowExpandable = '▸'
+vim.g.NERDTreeDirArrowCollapsible = '▾'
+vim.g.NERDTreeHighlightCursorline = 0
+
