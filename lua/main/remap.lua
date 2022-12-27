a605c2fbad9f0083c667e282cb6d518d84ab0b59
Darshan Parajuli <parajulidarshan@gmail.com>
2022-12-27 11:02:59 -0800

    Add mapping for prev/next buffer

--- lua/main/remap.lua
+++ lua/main/remap.lua
@@ -58,3 +58,7 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+
+vim.keymap.set('n', '<C-n>', vim.cmd.bnext)
+vim.keymap.set('n', '<C-p>', vim.cmd.bprev)
+
