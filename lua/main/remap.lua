f350b2376299fe165678b0d5b7d3290534a66089
Darshan Parajuli <parajulidarshan@gmail.com>
2022-12-27 11:01:16 -0800

    Add mapping for diagnostics

--- lua/main/remap.lua
+++ lua/main/remap.lua
@@ -53,3 +53,8 @@ vim.keymap.set("v", "K", "10k")
 
 vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
+
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
