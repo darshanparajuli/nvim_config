82a0ca0896bc45be3b807db4443b17cf8153eec1
Darshan Parajuli <parajulidarshan@gmail.com>
2022-12-26 16:05:56 -0800

    Add Kotlin highlighter for treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
  require'nvim-treesitter.configs'.setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'python', 'lua', 'rust', 'typescript', 'help' },
+  ensure_installed = { 'c', 'cpp', 'go', 'python', 'lua', 'rust', 'typescript', 'help', 'kotlin' },
 
   highlight = { enable = true },
   indent = { enable = true, disable = { 'python' } },
