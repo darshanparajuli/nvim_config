4f9261c93874c588394dfd7b49aff4756acd4e4d
Darshan Parajuli <parajulidarshan@gmail.com>
2023-01-08 13:46:57 -0800

    Enable auto-format-on-save for rust src files

--- lua/main/set.lua
+++ lua/main/set.lua
@@ -50,4 +50,8 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   pattern = { "*" },
   command = [[%s/\s\+$//e]],
 })
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  pattern = { "*.rs" },
+  command = [[lua vim.lsp.buf.formatting_sync()]],
+})
 
