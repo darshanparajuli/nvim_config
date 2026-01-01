1096a9030ab51959ecf4fff93ce53cf3b3225c64
Darshan Parajuli <parajulidarshan@gmail.com>
2025-12-31 17:07:24 -0800

    Fix resize keybindings

--- after/plugin/resize.lua
+++
@@ -1,4 +0,0 @@
-vim.keymap.set("n", "<C-K>", vim.cmd.CmdResizeUp, { silent = true })
-vim.keymap.set("n", "<C-J>", vim.cmd.CmdResizeDown, { silent = true })
-vim.keymap.set("n", "<C-H>", vim.cmd.CmdResizeLeft, { silent = true })
-vim.keymap.set("n", "<C-L>", vim.cmd.CmdResizeRight, { silent = true })
--- lua/main/remap.lua
+++ lua/main/remap.lua
@@ -61,3 +61,7 @@ vim.keymap.set("n", "<leader>t", function()
   vim.cmd("%s/\\s\\+$//e")
 end)
 
+vim.keymap.set("n", "<C-K>", vim.cmd.CmdResizeUp, { silent = true })
+vim.keymap.set("n", "<C-J>", vim.cmd.CmdResizeDown, { silent = true })
+vim.keymap.set("n", "<C-H>", vim.cmd.CmdResizeLeft, { silent = true })
+vim.keymap.set("n", "<C-L>", vim.cmd.CmdResizeRight, { silent = true })
